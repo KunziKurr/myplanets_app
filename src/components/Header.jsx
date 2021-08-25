69dd9a74344c4cb2db5cfaf1028ee2f24fd16302
Emmanue Korir <ekipyegon@co-opbank.co.ke>
2021-08-25 16:09:31 +0300

    Syntax Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 const Header = (props) => {
 
   const  handleClick =(_planet) =>{
@@ -12,15 +12,15 @@ const Header = (props) => {
             </div>
             <nav className="header_nav_nav">
                 <ul className="header_nav_nav_ul">
-                  <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("mercury")}}  className="header_nav_nav_ul_li_a">mercury</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("venus")}}  className="header_nav_nav_ul_li_a">venus</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("earth")}}  className="header_nav_nav_ul_li_a">earth</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("mars")}} className="header_nav_nav_ul_li_a">mars</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("jupiter")}} className="header_nav_nav_ul_li_a">jupiter</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("saturn")}} className="header_nav_nav_ul_li_a">saturn</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("uranus")}} className="header_nav_nav_ul_li_a">uranus</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("neptune")}} className="header_nav_nav_ul_li_a">neptune</a></li>
-                    <li className="header_nav_nav_ul_li"><a href="#" onClick={()=>{handleClick("pluto")}} className="header_nav_nav_ul_li_a">pluto</a></li>
+                  <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("mercury")}} >mercury</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("venus")}}  >venus</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("earth")}}  >earth</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("mars")}} >mars</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("jupiter")}} >jupiter</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("saturn")}} >saturn</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("uranus")}} >uranus</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("neptune")}} >neptune</li>
+                    <li className="header_nav_nav_ul_li" onClick={()=>{handleClick("pluto")}} >pluto</li>
                 </ul>   
             </nav>
         </header>
